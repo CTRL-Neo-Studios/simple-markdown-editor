e953deebf44c61ffe8ae4cfeb737129d9554dec1
Type-32 <87076491+Type-32@users.noreply.github.com>
2025-05-28 10:19:43 +0800

    chore: changed styling

--- app/utils/codemirror-rich-markdoc/highlightStyle.ts
+++ app/utils/codemirror-rich-markdoc/highlightStyle.ts
@@ -2,14 +2,16 @@ import { HighlightStyle } from '@codemirror/language';
 import { tags as t } from '@lezer/highlight';
 
 export default HighlightStyle.define([
-    { tag: t.heading1, fontWeight: 'bold', fontFamily: 'sans-serif', fontSize: '32px', textDecoration: 'none' },
-    { tag: t.heading2, fontWeight: 'bold', fontFamily: 'sans-serif', fontSize: '28px', textDecoration: 'none' },
-    { tag: t.heading3, fontWeight: 'bold', fontFamily: 'sans-serif', fontSize: '24px', textDecoration: 'none' },
-    { tag: t.heading4, fontWeight: 'bold', fontFamily: 'sans-serif', fontSize: '22px', textDecoration: 'none' },
-    { tag: t.link, fontFamily: 'sans-serif', textDecoration: 'underline', color: 'blue' },
-    { tag: t.emphasis, fontFamily: 'sans-serif', fontStyle: 'italic' },
-    { tag: t.strong, fontFamily: 'sans-serif', fontWeight: 'bold' },
-    { tag: t.monospace, fontFamily: 'monospace' },
-    { tag: t.content, fontFamily: 'sans-serif' },
-    { tag: t.meta, color: 'darkgrey' },
+    { tag: t.heading1, class: 'prose-cm-h1' },
+    { tag: t.heading2, class: 'prose-cm-h2' },
+    { tag: t.heading3, class: 'prose-cm-h3' },
+    { tag: t.heading4, class: 'prose-cm-h4' },
+    { tag: t.heading5, class: 'prose-cm-h5' },
+    { tag: t.heading6, class: 'prose-cm-h6' },
+    { tag: t.link, class: 'prose-cm-link' },
+    { tag: t.emphasis, class: 'prose-cm-emphasis' },
+    { tag: t.strong, class: 'prose-cm-strong' },
+    { tag: t.monospace, class: 'prose-cm-monospace' },
+    { tag: t.content, class: 'prose-cm-content' },
+    { tag: t.meta, class: 'prose-cm-meta' },
 ]);
